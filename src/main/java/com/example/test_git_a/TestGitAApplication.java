7dff3feee8bc6c00352d6bab5b1b038565e85d79
baiduhongchen <584615880@qq.com>
2019-07-23 22:40:40 +0800

    111

---
+++ src/main/java/com/example/test_git_a/TestGitAApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_git_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitAApplication.class, args);
+	}
+
+}
